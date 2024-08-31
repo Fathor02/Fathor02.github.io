dd3c270358430c68d6ad62bd0d5b3b60c6514159
Fathor02 <161577366+Fathor02@users.noreply.github.com>
2024-08-31 13:53:02 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed("#element", {
+    strings: [
+        "Web Developer",
+        "Web Desainer",
+        "Freelance",
+        "Grapich Desainer",
+        "UI/UX Desainer"
+    ],
+    typedSpeed: 110,
+    loop: true,
+    shuffle: true,
+    backSpeed: 100
+})
